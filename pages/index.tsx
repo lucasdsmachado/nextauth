4eb6055b02570ea313c78fb4d7d7a41aa8e89449
lucasdsmachado <briandsmachado@gmail.com>
2022-05-07 18:08:36 -0300

    redirect when user is logged in

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,5 @@
+import { GetServerSideProps } from 'next';
+import { parseCookies } from 'nookies';
 import { FormEvent, useState, useContext } from 'react';
 import { AuthContext } from '../contexts/AuthContext';
 import styles from '../styles/Home.module.css';
@@ -27,3 +29,19 @@ export default function Home() {
     </form>
   )
 }
+
+  export const getServerSideProps: GetServerSideProps = async(ctx) => {
+    const cookies = parseCookies(ctx);
+
+    if (cookies['nextauth.token']) {
+      return {
+        redirect: {
+          destination: '/dashboard',
+          permanent: false,
+        }
+      }
+    }
+    return {
+      props: {}
+    }
+  }
