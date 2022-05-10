6c9359ce7215cc5d9167dde68f23752d65876e4c
lucasdsmachado <briandsmachado@gmail.com>
2022-05-09 23:52:52 -0300

    return error when is in server env

--- services/api.ts
+++ services/api.ts
@@ -1,6 +1,7 @@
 import axios, { AxiosError } from 'axios';
 import { parseCookies, setCookie } from 'nookies';
 import { signOut } from '../contexts/AuthContext';
+import { AuthTokenError } from '../errors/AuthTokenError';
 
 let isRefreshing = false;
 let failedRequestsQueue = [];
@@ -71,8 +72,11 @@ export function setupAPIClient(ctx = undefined) {
         })
       })
     } else {
-      if (!isServer)
-      signOut();
+      if (!isServer) {
+        signOut();
+      } else {
+        return Promise.reject(new AuthTokenError())
+      }
     }
   }
 
