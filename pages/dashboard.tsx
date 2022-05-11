7419884b1f653674b1ce0c14884dcbd978646803
lucasdsmachado <briandsmachado@gmail.com>
2022-05-11 00:19:09 -0300

    create logout button

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -5,7 +5,7 @@ import { setupAPIClient } from "../services/api";
 import { withSSRAuth } from "../utils/withSSRAuth";
 
 export default function Dashboard() {
-  const { user } = useContext(AuthContext);
+  const { user, signOut } = useContext(AuthContext);
 
   useEffect(() => {
     api.get('/me')
@@ -14,8 +14,10 @@ export default function Dashboard() {
   }, []);
   return (
 
-    <h1>Dashboard Welcome {user?.email}</h1>
-
+    <>
+      <h1>Dashboard Welcome {user?.email}</h1>
+      <button onClick={signOut}>Sign out</button>
+    </>
   );
 }
 
